0f5c4a265a312e2bb6d59a671137e9d3e0845e60
rwoll <ross.wollman@gmail.com>
2016-04-11 20:35:03 -0700

    yelpapi env

--- chirp/views.py
+++ chirp/views.py
@@ -5,7 +5,15 @@ from yelpapi import YelpAPI
 
 from chirp import app
 
+yelp_api = YelpAPI(os.environ['YELP_KEY'], os.environ['YELP_SECRET'],
+                   os.environ['YELP_TOKEN'], os.environ['YELP_TOKEN_SECRET'])
 
 @app.route("/")
 def index():
+    try:
+        yelp_rs = yelp_api.search_query(location=request.args.get("location"))
     return render_template("index.html", name="Ross")
+
+@app.route("/search")
+def search():
+    return request.args.get("location")
